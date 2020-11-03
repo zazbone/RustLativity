599db8b7ca4e6b552ecdced0c475919f37eeac2e
zazbone <coczaz@gmail.com>
2020-11-03 10:04:28 +0100

    Index, IndexMut for LorentzMatrix

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -15,11 +15,23 @@ impl<T> Index<usize> for LorentzMatrix<T> {
     }
 }
 
+impl<T> IndexMut<usize> for LorentzMatrix<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
 
 impl<T> Index<(usize, usize)> for LorentzMatrix<T> {
     type Output = T;
 
     fn index(&self, lc: (usize, usize)) -> &Self::Output {
             &self.0[lc.0][lc.1]
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for LorentzMatrix<T> {
+    fn index_mut(&mut self, lc: (usize, usize)) -> &mut Self::Output {
+        &mut self.0[lc.0][lc.1]
     }
 }
